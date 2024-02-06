b4a856f1dedbeb400adfeeb4a4558d9b2f1fce50
arjunmalik11 <malia01@pfw.edu>
2024-02-05 22:53:16 -0500

    updated changes with fixed issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask
-from config import Config
 from dotenv import load_dotenv
+import os
 
+# Load environment variables
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object('config.Config')
     
     # Initialize plugins here SQLAlchemy or other
-    
+    from app.module_one import bp as module_one_bp
+    app.register_blueprint(module_one_bp)
     # Register Blueprints
     
     
